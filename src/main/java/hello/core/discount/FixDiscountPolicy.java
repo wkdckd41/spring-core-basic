7c248f80e9d88c956cb0527655b47b420f2f9e1d
wkdckd41 <wkdckd41@gmail.com>
2023-03-17 23:51:57 +0900

    주문과 할인도메인 실행과 테스트

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -9,10 +9,10 @@ public class FixDiscountPolicy implements DiscountPolicy {
 
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP) {
+        if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
         } else {
+            return 0;
         }
-        return 0;
     }
 }
