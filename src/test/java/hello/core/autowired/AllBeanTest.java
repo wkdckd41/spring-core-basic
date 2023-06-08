492b020f302262032fd0f62d078e849d253e1c76
wkdckd41 <wkdckd41@gmail.com>
2023-06-08 23:59:40 +0900

    조회한 빈이 모두 필요할때 List Map

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,28 @@
+package hello.core.autowired;
+
+import hello.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(DiscountService.class);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyMap = " + policyMap);
+        }
+    }
+}
