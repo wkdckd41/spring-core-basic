ced690e1ca02f38c2b78c048a648d1a5605a85e0
wkdckd41 <wkdckd41@gmail.com>
2023-06-09 23:50:48 +0900

    조회한 빈이 모두 필요할때 List Map

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -27,6 +27,11 @@ public class AllBeanTest {
 
         assertThat(discountService).isInstanceOf(DiscountService.class);
         assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+
     }
 
     static class DiscountService {
